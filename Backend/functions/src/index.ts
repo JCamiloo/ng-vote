075516f1b1a1f62846d546b63a56f2ea19c3d820
JCamiloo <jcamilo.osorio15@gmail.com>
2019-12-11 23:09:43 -0500

    adding credentials

--- Backend/functions/src/index.ts
+++ Backend/functions/src/index.ts
@@ -1,8 +1,12 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+const serviceAccount = require("./serviceAccountKey.json");
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://goty-f757d.firebaseio.com"
+});
+
+export const helloWorld = functions.https.onRequest((request, response) => {
+ response.send("Hello from Firebase!");
+});
