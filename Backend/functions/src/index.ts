c3733b3ffbc0036fc1f57ac7058703c1e8fc63f8
JCamiloo <jcamilo.osorio15@gmail.com>
2019-12-12 23:51:47 -0500

    backend deployed

--- Backend/functions/src/index.ts
+++ Backend/functions/src/index.ts
@@ -1,5 +1,8 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
+import * as express from 'express';
+import * as cors from 'cors';
+
 const serviceAccount = require("./serviceAccountKey.json");
 
 admin.initializeApp({
@@ -7,6 +10,44 @@ admin.initializeApp({
     databaseURL: "https://goty-f757d.firebaseio.com"
 });
 
-export const helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
+const db = admin.firestore();
+
+export const getGames = functions.https.onRequest( async(request, response) => {
+    const gotyRef = db.collection('goty');
+    const docsSnap = await gotyRef.get();
+    const game = docsSnap.docs.map(doc => doc.data());
+    response.json(game);
 });
+
+const app = express();
+app.use( cors({ origin: true }));
+
+app.get('/goty', async(req, res) => {
+    const gotyRef = db.collection('goty');
+    const docsSnap = await gotyRef.get();
+    const game = docsSnap.docs.map(doc => doc.data());
+    res.json(game);
+});
+
+app.post('/goty/:id', async(req, res) => {
+    const id = req.params.id;
+    const gameRef = db.collection('goty').doc(id);
+    const gameSnap = await gameRef.get();
+
+    if (!gameSnap.exists) {
+        res.status(404).json({
+            success: false,
+            message: id + ' does not exist'
+        });
+    } else {
+        const data = gameSnap.data() || { votes: 0 };
+        await gameRef.update({  votes: data.votes + 1 });
+
+        res.json({
+            success: true,
+            message: `Thanks for voting for ${data.name}`
+        });
+    }
+});
+
+export const api = functions.https.onRequest(app);
